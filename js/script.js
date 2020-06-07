c5e67c2d7e589c1499c90694b0d5c513a81f7c02
Yash Sawarn <yashsawarn1654@gmail.com>
2020-06-07 12:28:22 +0530

    working fine except for intervention message

--- js/script.js
+++ js/script.js
@@ -1,41 +1,37 @@
 var jcrop_api;
 var canvas1, canvas3;
 var cntxt1, cntxt3, stage;
-var tmpCanvas, tmpContxt
+//var tmpCanvas, tmpContxt
 
 $(function($) {
     canvas1 = $("#canvas2").get(0);
-    canvas3 = document.getElementById("canvas3");
-    tmpCanvas = document.createElement('canvas');
-    tmpContxt = tmpCanvas.getContext("2d");
+   // canvas3 = document.getElementById("canvas3");
+   // tmpCanvas = document.createElement('canvas');
+   // tmpContxt = tmpCanvas.getContext("2d");
     cntxt1 = canvas1.getContext("2d");
-    cntxt3 = canvas3.getContext("2d");
+    //cntxt3 = canvas3.getContext("2d");
     var img1 = new Image();
     img1.src = "../images/girl.png";
-    img1.onload = function(){  
+    img1.onload = function(){
+        $('#target1').Jcrop({
+            onSelect: makeTransparent
+          },
+          function(){
+              jcrop_api = this;
+          });  
         canvas1.width = document.body.clientWidth;
         canvas1.height = (552/984) * canvas1.width;
-        canvas3.width = canvas1.width;
-        canvas3.height = canvas1.height;
+        //canvas3.width = canvas1.width;
+       // canvas3.height = canvas1.height;
         cntxt1.drawImage(img1, 0 , 0, img1.width, img1.height, 0, 0, canvas1.width, canvas1.height);
-        cntxt3.drawImage(canvas1, 0,0, canvas1.width, canvas1.height, 0, 0, canvas3.width, canvas3.height)
-        tmpContxt.drawImage(canvas1, 0,0, canvas1.width, canvas1.height, 0, 0, canvas3.width, canvas3.height)
+       // cntxt3.drawImage(canvas1, 0,0, canvas1.width, canvas1.height, 0, 0, canvas3.width, canvas3.height)
+        //tmpContxt.drawImage(canvas1, 0,0, canvas1.width, canvas1.height, 0, 0, canvas3.width, canvas3.height)
         console.log("ATTACH")
     }
     var canvas2 = $("#canvas2").get(0);
     var img2 = new Image();
     img2.src = "../images/girl.png";
     var cntxt2 = canvas2.getContext("2d");
-    
-    document.getElementById("target1").onload = function() {
-
-        $('#target1').Jcrop({
-            onSelect: makeTransparent
-          },
-          function(){
-              jcrop_api = this;
-          });
-    } 
     //window.addEventListener("orientationchange", resizeCanvas); 
 });
 
@@ -61,8 +57,8 @@ function makeTransparent(c){
         }
     }
     cntxt1.putImageData(imageData,0 ,0);
-    cntxt3.putImageData(imageData, 0, 0);
-    tmpContxt.putImageData(imageData,0,0);
+    //cntxt3.putImageData(imageData, 0, 0);
+    //tmpContxt.putImageData(imageData,0,0);
     jcrop_api.release();
 };
 
